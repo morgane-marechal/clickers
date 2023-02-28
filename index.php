3069d3571b9ad4d243430352c48b00697746451c
morgane-marechal <morgane.marechal@laplateforme.io>
2023-02-28 11:26:51 +0100

    version1 jouable

--- index.php
+++ index.php
@@ -12,25 +12,24 @@
         </header>
         <div id="center">
             <button id="click-me" onclick="newClick()">Click</button>
-            <div id="display-score">
-        </div>
-
-        <div id="bonus">
-            <div class="card">
-                <button id="smallAdd" onclick="small()">Petit Add</button>
-                <p class="prix">Prix du bouton: <span id="prixsmall"></span> </p>
-            </div>
-            <div class="card">
-                <button id="mediumAdd" onclick="medium()">Medium Add</button>
-                <p class="prix">Prix du bouton: <span id="prixmedium"></span></p>
-            </div>
-            <div class="card">
-                <button id="bigAdd" onclick="big()">Big Add</button>
-                <p class="prix">Prix du bouton: <span id="prixbig"></p>
-            </div>
-        </div>
-
-
+                <div id="display-score"></div>
+                <div id="bonus">
+                    <div class="card" title="valeur du click +1">
+                        <button id="smallAdd" onclick="small()">Petit Add</button>
+                        <p class="prix">Prix du bouton <br><span id="prixsmall"></span> ck</p>
+                    </div>
+                    <div class="card" title="incrément/S x 2">
+                        <button id="mediumAdd" onclick="medium()">Medium Add</button>
+                        <p class="prix">Prix du bouton <br><span id="prixmedium"></span> ck</p>
+                    </div>
+                    <div class="card" title="incrément/S x 3">
+                        <button id="bigAdd" onclick="big()">Big Add</button>
+                        <p class="prix">Prix du bouton <br><span id="prixbig"></span> ck</p>
+                    </div>
+                </div>
+                <div id="reset">
+                    <button id="btnreset" onclick="reset()">Reset</button>
+                </div>
         </div>
         <footer>
         <script defer type="text/javascript" src="script.js"></script>
--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const score = document.getElementById("display-score");
+const resetScore = document.getElementById("btnreset");
 const btnClick = document.getElementById("click-me");
 const btnSmall = document.getElementById("smallAdd");
 const btnMedium = document.getElementById("mediumAdd");
@@ -7,13 +8,15 @@ let prixSmall = document.getElementById("prixsmall");
 let prixMedium = document.getElementById("prixmedium");
 let prixBig = document.getElementById("prixbig");
 
-document.getElementById("smallAdd").disabled = true;
+document.getElementById("smallAdd").disabled = true; //desactive items trop chers
 document.getElementById("mediumAdd").disabled = true;
 document.getElementById("bigAdd").disabled = true;
-
+document.getElementById("display-score").innerHTML = 0;
 score.innerHTML = 0;
-let valueClick = 1.0;
+let valueClick = 1;
 let scoreClick = 0;
+let mediumActive=false; //variable pour voir si setInterval medium fonctionne
+let bigActive=false; //variable pour voir si setInterval medium fonctionne
 
 //----------fonctions pour augmenter la valeur du click------
 
@@ -21,12 +24,12 @@ function newClick() {
     scoreClick += valueClick;
     document.getElementById("display-score").innerHTML = scoreClick;
     check();
-    console.log("hello " + disabled_btn_small);
 }
 
 //----------fonction régulièrement appeler pour vérifier si on bloque bouton ou pas------
 
 function check() {
+    addLocalStorage();
     if (scoreClick >= disabled_btn_small) {
         document.getElementById("smallAdd").disabled = false;
     } else if (scoreClick <= disabled_btn_small) {
@@ -44,7 +47,6 @@ function check() {
     } else if (scoreClick <= disabled_btn_big) {
         document.getElementById("bigAdd").disabled = true;
     }
-
 }
 
 //---------------variable et fonction pour le bouton le moins chers-------------
@@ -52,27 +54,20 @@ function check() {
 let disabled_btn_small = 10;
 prixSmall.innerHTML = disabled_btn_small;
 
-let nb_small = 0; //nombre de fois où j'appelle la fonction small
-
-
 function small() {
-    valueClick = Math.round(valueClick * 1.6);//la valeure du click augmente
-    //console.log("value click "+valueClick);
+    valueClick = valueClick + 1;//la valeure du click augmente
     scoreClick = scoreClick - disabled_btn_small;//on soustrait prix du click
     document.getElementById("display-score").innerHTML = scoreClick;
     disabled_btn_small = disabled_btn_small * 2; //prix du bouton qu augmente
-    console.log("prix bouton: " + disabled_btn_small)
     prixSmall.innerHTML = disabled_btn_small;
-    nb_small++;
     check(); // on check pour désactiver boutons trop chers
 }
 
 //---------------variable et fonction pour le bouton moyen chers-------------
 
-let valueMedium = 2;
+let valueMedium = 1;
 let disabled_btn_medium = 100;
 prixMedium.innerHTML = disabled_btn_medium;
-let nb_medium = 0; //nombre de fois où j'appelle la fonction medium
 
 function mediumInc() {
     scoreClick = scoreClick + valueMedium;
@@ -83,10 +78,16 @@ function mediumInc() {
 function medium() {
     intervalMedium = setInterval(mediumInc, 1000);
     scoreClick = scoreClick - disabled_btn_medium;//on soustrait prix du click
-    disabled_btn_medium = disabled_btn_medium * 2;
+    disabled_btn_medium = disabled_btn_medium * 3;
     prixMedium.innerHTML = disabled_btn_medium;
-    valueMedium = Math.round(valueMedium * 1.6);//augmente l'incrémentation chaque fois qu'on clique
-    nb_medium++;
+    valueMedium = valueMedium * 2;//augmente l'incrémentation chaque fois qu'on clique
+    mediumActive=true;
+}
+
+function medium_refresh(){
+    intervalMedium = setInterval(mediumInc, 1000);
+    prixMedium.innerHTML = disabled_btn_medium;
+    mediumActive=true;
 }
 
 //---------------variable et fonction pour bouton le plus chers-------------
@@ -94,8 +95,6 @@ function medium() {
 let valueBig = 10;//valeur qui va être multipliée quand on clique sur big()
 let disabled_btn_big = 1000;
 prixBig.innerHTML = disabled_btn_big;
-let nb_big = 0; //nombre de fois où j'appelle la fonction big
-
 
 function bigInc() {
     scoreClick = scoreClick + valueBig;
@@ -106,8 +105,103 @@ function bigInc() {
 function big() {
     intervalBig = setInterval(bigInc, 1000);
     scoreClick = scoreClick - disabled_btn_big;//on soustrait prix du click
-    disabled_btn_big = disabled_btn_big * 2;
+    disabled_btn_big = disabled_btn_big * 5;
     prixBig.innerHTML = disabled_btn_big;
-    valueBig = Math.round(valueBig * 2);
-    nb_big++;
+    valueBig = Math.round(valueBig * 3);
+    bigActive=true;
 }
+function big_refresh() {
+    intervalBig = setInterval(bigInc, 1000);
+    prixBig.innerHTML = disabled_btn_big;
+    bigActive=true;
+}
+
+//---------------fonction pour rentrer valeurs en local.storage-------------
+
+function addLocalStorage() {
+        var strScore = scoreClick.toString();//données sur le score
+        console.log("score "+strScore);
+        localStorage.setItem('score', strScore);       
+        localStorage.setItem('valueClick', valueClick);
+        localStorage.setItem('disabled_btn_small', disabled_btn_small);
+        localStorage.setItem('disabled_btn_medium', disabled_btn_medium);
+        localStorage.setItem('disabled_btn_big', disabled_btn_big);
+        localStorage.setItem('valueMedium', valueMedium);
+        localStorage.setItem('valueBig', valueBig);
+        localStorage.setItem('bigActive', bigActive);
+        localStorage.setItem('mediumActive', mediumActive);
+        console.log(localStorage);
+}
+
+function getLocalStorage(){
+    if (localStorage.getItem('score') != null) {
+        console.log("get score "+localStorage.getItem('score'));
+        scoreClick=localStorage.getItem('score');
+        scoreClick = parseInt(scoreClick);
+    } 
+    if (localStorage.getItem('valueClick') != null) {
+        valueClick=localStorage.getItem('valueClick');
+        valueClick = parseInt(valueClick);
+    } 
+    if(localStorage.getItem('disabled_btn_small') != null) {
+        disabled_btn_small=localStorage.getItem('disabled_btn_small');
+        disabled_btn_small = parseInt(disabled_btn_small);
+        prixSmall.innerHTML = disabled_btn_small;
+    }
+    if(localStorage.getItem('disabled_btn_medium') != null) {
+        disabled_btn_medium=localStorage.getItem('disabled_btn_medium');
+        disabled_btn_medium = parseInt(disabled_btn_medium);
+        prixMedium.innerHTML = disabled_btn_medium;
+    }
+    if(localStorage.getItem('disabled_btn_big') != null) {
+        disabled_btn_big=localStorage.getItem('disabled_btn_big');
+        disabled_btn_big = parseInt(disabled_btn_big);
+        prixBig.innerHTML = disabled_btn_big;
+    }
+    if(localStorage.getItem('valueMedium') != null) {
+        valueMedium=localStorage.getItem('valueMedium');
+        valueMedium = parseInt(valueMedium);
+    }
+    if(localStorage.getItem('valueBig') != null) {
+        valueBig=localStorage.getItem('valueBig');
+        valueBig = parseInt(valueBig);
+    }
+    if(localStorage.getItem('mediumActive') != null) {
+        mediumActive=localStorage.getItem('mediumActive');
+        if(mediumActive==="true"){
+            medium_refresh();
+        }
+    }
+
+    if(localStorage.getItem('bigActive') != null) {
+        bigActive=localStorage.getItem('bigActive');
+        if(bigActive==="true"){
+            big_refresh();
+        }
+    }
+}
+
+window.onload = (event) => {
+    getLocalStorage()
+    document.getElementById("display-score").innerHTML = scoreClick
+    check();
+};
+
+function reset() {
+    localStorage.clear();
+    valueClick = 1;
+    scoreClick = 0;
+    mediumActive=false; 
+    bigActive=false; 
+    disabled_btn_small = 10;
+    prixSmall.innerHTML = disabled_btn_small;
+    valueMedium = 2;
+    disabled_btn_medium = 100;
+    prixMedium.innerHTML = disabled_btn_medium;
+    valueBig = 10;
+    disabled_btn_big = 1000;
+    prixBig.innerHTML = disabled_btn_big;
+    score.innerHTML = 0;
+    clearInterval(intervalMedium);
+    clearInterval(intervalBig);
+} 
